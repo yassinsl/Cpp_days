e8eb91e01265d54c834a0a91af23f6005be4ece3
yassinsl <slawiyassin152@gmail.com>
2025-12-20 14:39:39 +0100

    Update

--- C05_finished/ex02/AForm.hpp
+++ C05_finished/ex02/AForm.hpp
@@ -6,15 +6,60 @@ typedef string std::string;
 class AForm{
   protected:
       string _name;
-      string _target;
       bool _signed_status;
-      const int grade_sign;
-      const int grade_execute;
+      const int _grade_sign;
+      const int _grade_execute;
   public:
+      AForm(string name, bool singed_status, int _grade_sign, int _grade_execute): _name(name), 
       void execute(Bureaucrat const &executor);
       virtual void executeAction() const = 0;
 };
 class ShrubberyCreationForm() : public AForm{
+    private:
+      const string _target;
     public:
       ShrubberyCreationForm();
+      void executeAction() const;
+}
+ShrubberyCreationForm::ShrubberyCreationForm(string target):AForm("Shrubbry", false,145,137), _target(target){}
+
+void ShrubberyCreationForm::executeAction() const
+{
+    const int Total_tree_height = 20;
+    std::ofstream MyFile(this->_target + "_shrubbery");
+
+    if (!MyFile.is_open())
+    {
+        std::cerr << "Cannot open file!" << std::endl;
+        return;
+    }
+    char symbols[] = { '*', 'o', 'O' };
+    int symbol_index = 0;
+
+    for (int i = 0; i < Total_tree_height; i++)
+    {
+        int left_space = (Total_tree_height - 1) - i;
+        int symbol_count = (2 * i) + 1;
+        for (int j = 0; j < left_space; j++)
+            MyFile << " ";
+        for (int j = 0; j < symbol_count; j++)
+        {
+            MyFile << symbols[symbol_index];
+            symbol_index = (symbol_index + 1) % 3;
+        }
+        MyFile << "\n";
+    }
+    int trunk_height = 3;
+    int trunk_width = 1;
+    int trunk_space = Total_tree_height - 1;
+
+    for (int i = 0; i < trunk_height; i++)
+    {
+        for (int j = 0; j < trunk_space; j++)
+            MyFile << " ";
+        for (int j = 0; j < trunk_width; j++)
+            MyFile << "|";
+        MyFile << "\n";
+    }
+    MyFile << "\nMerry Christmas!\n";
 }
